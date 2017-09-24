1725a41f7d0e52df9f87f9716cef4d519901f666
Guillermo Recalde <guirec92@gmail.com>
2017-09-23 21:21:40 -0300

    Flask: add /api on routing path

--- flask_webserver/main.py
+++ flask_webserver/main.py
@@ -12,11 +12,11 @@ from resources.HighProcessingResponse import HighProcessingResponse
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(ShortResponse, '/short')
-api.add_resource(FastResponse, '/fast')
-api.add_resource(SlowResponse, '/slow')
-api.add_resource(StaticResponse, '/static')
-api.add_resource(HighProcessingResponse, '/high-processing')
+api.add_resource(ShortResponse, '/api/short')
+api.add_resource(FastResponse, '/api/fast')
+api.add_resource(SlowResponse, '/api/slow')
+api.add_resource(StaticResponse, '/api/static')
+api.add_resource(HighProcessingResponse, '/api/high-processing')
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 7573))
